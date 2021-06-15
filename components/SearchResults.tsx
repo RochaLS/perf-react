ed98d88c6d0edb526cd9bc1a702d9c9114d28e4c
Lucas Rocha <ld.rocha@hotmail.com>
2021-06-14 17:35:56 -0700

    format data

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -2,9 +2,11 @@ import { ProductItem } from './ProductItem'
 import { useMemo } from 'react'
 
 interface SearchResultsProps {
+  totalPrice: number;
   results: Array<{
     id: number;
     price: number;
+    priceFormatted: string;
     title: string;
   }>
 
